d382f044cec7a34abf036bc8a28e2a176b7238c9
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2019-03-18 16:12:51 +0300

    Configured webpack-dev-server and added base application structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>app</div>
+        );
+    }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './public/build'),
-    publicPath: 'build/',
-    filename: 'bundle.js'
-  }
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, './public/build'),
+		publicPath: 'build/',
+		filename: 'bundle.js',
+		sourceMapFilename: 'bundle.js.map'
+	},
+	devServer: {
+		contentBase: path.join(__dirname, './public'),
+		filename: 'index.html',
+		compress: true,
+		port: 8097
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+					}
+				]
+			}
+		]
+	},
 };
