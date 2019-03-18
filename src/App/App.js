ae3d280ab27e8f3767190253d5fc271b4b6211a2
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2019-03-18 18:21:56 +0300

    added style to app js

--- src/App/App.js
+++ src/App/App.js
@@ -5,10 +5,12 @@ import Header from '../components/Header/Header';
 import Body from '../components/Body/Body';
 import Footer from '../components/Footer/Footer';
 
+import '../App/app.less'
+
 class App extends Component{
     render(){
         return(
-            <div>
+            <div className="app">
                 <Header />
                 <Body />
                 <Footer />
