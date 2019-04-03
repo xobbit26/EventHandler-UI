88d1428e5cd25066ea382982421591addc2621c6
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2019-04-03 13:15:32 +0300

    continue restructuring the application

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Switch, Route } from 'react-router';
+import { Switch, Route } from 'react-router-dom';
+
 import Sender from '../components/sender/sender';
 import Login from '../components/login/login';
+import Administration from '../components/administration/administration';
 
 import '../styles/app.less';
 
-class App extends Component{
-    render(){
-        return(
-            <Router>
-                <Switch>
-                    <Route exact path="/" component = {Sender}/>
-                    <Route path="/login" component = {Login}/>
-                </Switch>
-            </Router>
+class App extends Component {
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={Sender} />
+                <Route path="/login" component={Login} />
+                <Route path="/administration" component={Administration} />
+            </Switch>
         );
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Switch } from 'react-router'; 
 
-import Sender from '../src/components/sender/sender';
-import Login from '../src/components/login/login';
-import Administration from '../src/components/administration/administration';
+import App from '../src/App/App';
 
 ReactDOM.render(
-	<HashRouter>
-		<Switch>
-            <Route exact path="/" component = {Sender}/>
-            <Route path="/login" component = {Login}/>
-			<Route path="/administration" component = {Administration}/>
-        </Switch>
-    </HashRouter>,
+	<Router>
+        <App />
+    </Router>,
 	document.getElementById('app')
 );
