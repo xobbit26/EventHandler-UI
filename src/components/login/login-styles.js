40cf8d7ba651f62570f947f4a4058ffb5476bc0a
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2019-04-08 13:19:17 +0300

    fixed conflict

--- src/components/login/login-styles.js
+++ src/components/login/login-styles.js
@@ -2,12 +2,8 @@ const loginStyles = theme => ({
     root: {
         width: '100%',
         height: '100%',
-<<<<<<< HEAD
-        width: 900,
-=======
         width: 800,
         minWidth: 400,
->>>>>>> feature/making_sender
         borderRadius: 5,
         padding: 10,
         backgroundColor: theme.palette.background.paper
