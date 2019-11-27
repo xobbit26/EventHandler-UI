1a158618ec5e3c2e1ac71996bb9ab8e29b1a5e66
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2019-11-27 13:48:58 +0300

    changed api url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8096';
+export const API_URL = 'http://localhost:5000';
 //export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:8081' : '';
