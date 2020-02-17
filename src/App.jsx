d2ddae45691582da7f0b6e744207f4f3b39f29fe
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2020-02-17 19:44:47 +0300

    changer a path for import layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Layout from '../components/layout/Layout';
+import Layout from './components/layout/Layout';
 
 class App extends Component {
     constructor(props) {
