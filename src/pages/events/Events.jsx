77ea1988b2c9a93bae35cfaef319bb400c043217
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2020-03-02 18:26:52 +0300

    using hooks in Events component

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import Paper from '@material-ui/core/Paper';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
 import PropTypes from 'prop-types';
 import i18next from 'i18next';
 
@@ -10,95 +9,157 @@ import { REQUEST_EVENTS_URL, api } from '../../api/api';
 import { gridConstants } from '../../constants/gridConstants';
 import Grid from '../../components/grid/Grid';
 
-import { withStyles } from '@material-ui/core/styles';
 import eventListStyles from './events-styles';
 
-class Events extends React.Component {
+function Events() {
+    const dispatch = useDispatch();
 
-    constructor(props) {
-        super(props);
+    const [gridOptions, setGridOptions] = useState({
+        page: 0,
+        rowsPerPage: gridConstants.defaultItemsPerPage,
+        orderBy: 'applyDateTime',
+        order: 'asc'
+    });
 
-        this.state = {
-            grid: {
-                page: 0,
-                rowsPerPage: gridConstants.defaultItemsPerPage,
-                orderBy: 'applyDateTime',
-                order: 'asc'
-            }
-        }
+    useEffect(() => {
+        const { page, rowsPerPage, orderBy, order } = gridOptions;
+        requestEventsGridData(page, rowsPerPage, orderBy, order);
+    }, []);
 
-        this.requestEventsGridData = this.requestEventsGridData.bind(this);
-    };
+    const { eventsGridData } = useSelector(state => ({
+        eventsGridData: state.events.get('eventsGridData').toJS()
+    }));
 
-    componentDidMount() {
-        const { page, rowsPerPage, orderBy, order } = this.state.grid;
-        this.requestEventsGridData(page, rowsPerPage, orderBy, order);
-    }
-
-    requestEventsGridData(page, itemsPerPage, orderBy, order) {
+    function requestEventsGridData(page, rowsPerPage, orderBy, order) {
 
         //TODO: After creating loader it needt to get language parameter from i18next api
         console.log("i18next.language", i18next.language);
         const language = 'en';
 
-        const skip = page * itemsPerPage;
-        const url = `${REQUEST_EVENTS_URL}/${language}?skip=${skip}&take=${itemsPerPage}&orderBy=${orderBy}&direction=${order}`;
+        const skip = page * rowsPerPage;
+        const url = `${REQUEST_EVENTS_URL}/${language}?skip=${skip}&take=${rowsPerPage}&orderBy=${orderBy}&direction=${order}`;
 
         api.get(url)
-            .then((data) => {
-                this.props.requestEvents(data);
-            })
-            .then(() => {
-                this.setState({
-                    grid: {
-                        rowsPerPage: itemsPerPage,
-                        page: page,
-                        orderBy: orderBy,
-                        order: order
-                    }
-                });
-            });
-    }
+            .then((data) => { dispatch(requestEvents(data)); })
+            .then(() => { setGridOptions({ rowsPerPage, page, orderBy, order }); });
+    };
 
-    render() {
-        const { page, rowsPerPage, orderBy, order } = this.state.grid;
-        const { classes, eventsGridData } = this.props;
-        const { isGridEmpty, data, columns, totalItems } = eventsGridData;
+    const { page, rowsPerPage, orderBy, order } = gridOptions;
+    const { isGridEmpty, data, columns, totalItems } = eventsGridData;
+    const classes = eventListStyles();
 
-        return (
-            <Paper className={classes.root}>
-                {!isGridEmpty &&
-                    <Grid columns={columns}
-                        data={data}
-                        totalItems={totalItems}
-                        onChangePage={this.requestEventsGridData}
-                        onChangeRowsNumber={this.requestEventsGridData}
-                        onSort={this.requestEventsGridData}
-                        page={page}
-                        rowsPerPage={rowsPerPage}
-                        withPaging={true}
-                        orderBy={orderBy}
-                        order={order} />
-                }
-            </Paper>
-        )
-    }
-};
+    return (
+        <Paper className={classes.root}>
+            {!isGridEmpty &&
+                <Grid columns={columns}
+                    data={data}
+                    totalItems={totalItems}
+                    onChangePage={requestEventsGridData}
+                    onChangeRowsNumber={requestEventsGridData}
+                    onSort={requestEventsGridData}
+                    page={page}
+                    rowsPerPage={rowsPerPage}
+                    withPaging={true}
+                    orderBy={orderBy}
+                    order={order} />
+            }
+        </Paper>
+    )
+}
 
 Events.propTypes = {
     eventsGridData: PropTypes.object,
     requestEvents: PropTypes.func
 }
 
-const mapStateToProps = (state) => ({
-    eventsGridData: state.events.get('eventsGridData').toJS()
-});
+export default Events;
 
-const mapDispatchToProps = {
-    requestEvents
-}
+// class Events extends React.Component {
 
-export default compose(
-    connect(mapStateToProps, mapDispatchToProps),
-    withStyles(eventListStyles)
-)(Events);
+//     constructor(props) {
+//         super(props);
+
+//         this.state = {
+//             grid: {
+//                 page: 0,
+//                 rowsPerPage: gridConstants.defaultItemsPerPage,
+//                 orderBy: 'applyDateTime',
+//                 order: 'asc'
+//             }
+//         }
+
+//         this.requestEventsGridData = this.requestEventsGridData.bind(this);
+//     };
+
+//     componentDidMount() {
+//         const { page, rowsPerPage, orderBy, order } = this.state.grid;
+//         this.requestEventsGridData(page, rowsPerPage, orderBy, order);
+//     }
+
+//     requestEventsGridData(page, itemsPerPage, orderBy, order) {
+
+//         //TODO: After creating loader it needt to get language parameter from i18next api
+//         console.log("i18next.language", i18next.language);
+//         const language = 'en';
+
+//         const skip = page * itemsPerPage;
+//         const url = `${REQUEST_EVENTS_URL}/${language}?skip=${skip}&take=${itemsPerPage}&orderBy=${orderBy}&direction=${order}`;
+
+//         api.get(url)
+//             .then((data) => {
+//                 this.props.requestEvents(data);
+//             })
+//             .then(() => {
+//                 this.setState({
+//                     grid: {
+//                         rowsPerPage: itemsPerPage,
+//                         page: page,
+//                         orderBy: orderBy,
+//                         order: order
+//                     }
+//                 });
+//             });
+//     }
+
+//     render() {
+//         const { page, rowsPerPage, orderBy, order } = this.state.grid;
+//         const { classes, eventsGridData } = this.props;
+//         const { isGridEmpty, data, columns, totalItems } = eventsGridData;
+
+//         return (
+//             <Paper className={classes.root}>
+//                 {!isGridEmpty &&
+//                     <Grid columns={columns}
+//                         data={data}
+//                         totalItems={totalItems}
+//                         onChangePage={this.requestEventsGridData}
+//                         onChangeRowsNumber={this.requestEventsGridData}
+//                         onSort={this.requestEventsGridData}
+//                         page={page}
+//                         rowsPerPage={rowsPerPage}
+//                         withPaging={true}
+//                         orderBy={orderBy}
+//                         order={order} />
+//                 }
+//             </Paper>
+//         )
+//     }
+// };
+
+// Events.propTypes = {
+//     eventsGridData: PropTypes.object,
+//     requestEvents: PropTypes.func
+// }
+
+// const mapStateToProps = (state) => ({
+//     eventsGridData: state.events.get('eventsGridData').toJS()
+// });
+
+// const mapDispatchToProps = {
+//     requestEvents
+// }
+
+// export default compose(
+//     connect(mapStateToProps, mapDispatchToProps),
+//     withStyles(eventListStyles)
+// )(Events);
--- src/pages/events/events-styles.js
+++ src/pages/events/events-styles.js
@@ -1,8 +1,10 @@
-const eventListStyles = theme => ({
+import { makeStyles } from '@material-ui/core/styles';
+
+const eventListStyles = makeStyles(theme => ({
     root: {
         width: '100%',
         marginTop: theme.spacing(),
     }
-});
+}));
 
 export default eventListStyles;
