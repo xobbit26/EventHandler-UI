ef6b835efab60fe0996874433f2612d08eeeadec
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2020-03-04 17:21:27 +0300

    added react toastify and put in to api calls

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,17 @@
 import i18n from '../configuration/i18n';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { API_URL } from '../configuration/config';
 export const CREATE_EVENT_URL = `${API_URL}/api/event`;
 export const REQUEST_EVENTS_URL = `${API_URL}/api/event/grid-data`;
+
+// Call it once in your app. At the root of your app is the best place
+toast.configure({
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: true
+});
 
 const headers = {
     'content-type': 'application/json',
@@ -43,18 +52,21 @@ const handleErrors = (response) => {
     console.log(i18n.t('TRANSLATIONS IMPORT !!!!'));
     return response.ok
         ? response
-        : response.json()
+        : response.text()
             .then((e) => throwNetworkError(e, response))
-            .catch((e) => throwNetworkError(e, response));
+            //.catch((e) => throwNetworkError(e, response));
 }
 
 const throwNetworkError = (remoteError, response) => {
     switch (response.status) {
-        case 401: throw new AuthError(remoteError.message);
-        case 403: throw new ForbiddenError(remoteError.message);
-        case 404: throw new NotFoundError(remoteError.message);
-        case 400: throw new BadRequestError(remoteError.message);
-        default: throw Error(remoteError.message);
+        // case 401: throw new AuthError(remoteError.message);
+        // case 403: throw new ForbiddenError(remoteError.message);
+        // case 404: throw new NotFoundError(remoteError.message);
+        // case 400: throw new BadRequestError(remoteError.message);
+        default: {
+            toast.error(i18n.t('ErrorMessasge_InternalServer'));
+            throw new Error(remoteError.message);
+        };
     }
 };
 
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -71,11 +71,18 @@ module.exports = {
                 ],
             },
             {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+            {
                 test: /\.json$/,
                 loader: "json-loader",
             }
         ]
-    },plugins: [
+    }, plugins: [
         new HtmlWebPackPlugin({
             template: './public/index.html',
             filename: './index.html',
